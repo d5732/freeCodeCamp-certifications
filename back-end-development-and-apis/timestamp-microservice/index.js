857c8e6ef7dc75bfd357597b09e25d8b9edecb5d
d5732 <denwa.dev@gmail.com>
2022-12-18 20:45:11 -0600

    feat: GET /api, GET /api/:time

--- back-end-development-and-apis/timestamp-microservice/index.js
+++ back-end-development-and-apis/timestamp-microservice/index.js
@@ -1,32 +1,37 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+function timeHandler(req, res) {
+    const time = req.params.time;
+    let d = new Date(time);
+    if (d == "Invalid Date") {
+        d = new Date(Number(time));
+    }
+    if (d == "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+    } else {
+        res.json({ unix: d.valueOf(), utc: d.toUTCString() });
+    }
+}
+app.get("/api/:time", timeHandler);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+function nowHandler(req, res) {
+    const d = new Date();
+    res.json({ unix: d.valueOf(), utc: d.toUTCString() });
+}
+app.get("/api/", nowHandler);
 
-
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
